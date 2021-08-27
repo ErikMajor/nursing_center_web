85b2001e0d9f47d5a5374a1985b74e4be7086d38
YuCongGe <1025790892@qq.com>
2021-08-27 20:47:20 +0800

    commit by ge yu cong

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 
 <style>
   /** 清除内外边距 **/
+  /** **/
   body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, /* structural elements 结构元素 */
   dl, dt, dd, ul, ol, li, /* list elements 列表元素 */
   pre, /* text formatting elements 文本格式元素 */
