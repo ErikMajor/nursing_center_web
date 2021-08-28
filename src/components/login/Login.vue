9365e581978cc345295ed6552e060e6f8da42014
陈家乐 <18204110@nchu.edu.cn>
2021-08-28 11:59:51 +0800

    更改了login界面

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -8,6 +8,14 @@
                 <el-form-item label="密码" prop="password">
                     <el-input v-model="ruleForm.password"></el-input>
                 </el-form-item>
+                <el-select v-model="value" placeholder="请选择">
+                    <el-option
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                    </el-option>
+                </el-select>
                 <el-form-item>
                     <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
                 </el-form-item>
@@ -24,15 +32,32 @@
                     username: '', //用户名
                     password: ''  //密码
                 },
-                    rules: {
-                        username: [
-                            { required: true, message: '请输入用户名', trigger: 'blur' },
-                        ],
-                        password: [
-                            { required: true, message: '请输入密码', trigger: 'change' },
-                            { min: 3, max: 6, message: '长度在 3 到 6 个字符', trigger: 'blur' }
-                        ],
-                    }
+                rules: {
+                    username: [
+                        { required: true, message: '请输入用户名', trigger: 'blur' },
+                    ],
+                    password: [
+                        { required: true, message: '请输入密码', trigger: 'change' },
+                        { min: 3, max: 6, message: '长度在 3 到 6 个字符', trigger: 'blur' }
+                    ],
+                },
+                options: [{
+                    value: '选项1',
+                    label: '黄金糕'
+                }, {
+                    value: '选项2',
+                    label: '双皮奶'
+                }, {
+                    value: '选项3',
+                    label: '蚵仔煎'
+                }, {
+                    value: '选项4',
+                    label: '龙须面'
+                }, {
+                    value: '选项5',
+                    label: '北京烤鸭'
+                }],
+                value: ''
                 }
         },
         components: {
