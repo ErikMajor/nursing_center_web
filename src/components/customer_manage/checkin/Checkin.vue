3acef90e32e2b3062e02bc2fddf9dd5a7580feb2
YuCongGe <1025790892@qq.com>
2021-08-30 22:32:52 +0800

    checkin模块更新

--- src/components/customer_manage/checkin/Checkin.vue
+++ src/components/customer_manage/checkin/Checkin.vue
@@ -1,62 +1,55 @@
 <template>
   <div>
     <el-container>
-      <el-header style="text-align: center;">
-        <el-autocomplete
-            v-model="state"
-            :fetch-suggestions="queryCustomer"
-            placeholder="输入客户名称"
-            @select="handleSelect"
-        ></el-autocomplete>
+      <el-header>
+        <el-input
+            v-model="name"
+            clearable
+            placeholder="请输入客户的名字"
+            style="width: 40%;margin: 1%;padding-left: 50%"
+            @input="search"
+        ></el-input>
       </el-header>
+      <el-main>
+        <customer :customer="this.name"/>
+      </el-main>
     </el-container>
   </div>
 </template>
 
 <script>
-export default {
+import CustomerCard from "@/components/customer_manage/checkin/CustomerCard";
+import axios from "axios";
 
+export default {
+  components: {
+    customer: CustomerCard,
+  },
   data() {
     return {
-      customers: [],
-      state: '',
+      rows: [],
+      name: '',
       timeout: null
     }
   },
   methods: {
-    loadAll() {
-      return [
-        {"value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号"},
-        {"value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号"},
-        {"value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113"},
-        {"value": "泷千家(天山西路店)", "address": "天山西路438号"},
-        {"value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101"},
-        {"value": "贡茶", "address": "上海市长宁区金钟路633号"},
-        {"value": "豪大大香鸡排超级奶爸", "address": "上海市嘉定区曹安公路曹安路1685号"},
-        {"value": "茶芝兰（奶茶，手抓饼）", "address": "上海市普陀区同普路1435号"},
-        {"value": "十二泷町", "address": "上海市北翟路1444弄81号B幢-107"},
-        {"value": "星移浓缩咖啡", "address": "上海市嘉定区新郁路817号"}
-      ]
-    },
-    queryCustomer(name, cb) {
-      var customers = this.customers
-      var results = name ? customers.filter(this.createStateFilter(name)) : customers
-      clearTimeout(this.timeout)
-      this.timeout = setTimeout(() => {
-        cb(results)
-      }, 3000 * Math.random())
-    },
-    createStateFilter(name) {
-      return (state) => {
-        return (state.value.toLowerCase().indexOf(name.toLowerCase()) === 0)
-      }
-    },
-    handleSelect(item) {
-      console.log(item)
+    async search() {
+      let data = []
+      await axios.get('http://localhost:8081/checkin/queryByName/1',{
+        params: {
+          name: this.name.toString()
+        }
+      })
+      .then(function (response) {
+        data = response.data.customers
+        console.log(response)
+      })
+      this.rows = data
+      console.log(this.rows[0])
     }
   },
   mounted() {
-    this.customers = this.loadAll()
+
   }
 }
 </script>
---
+++ src/components/customer_manage/checkin/CustomerCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-card
+      :body-style="{padding: '1%', margin: '1%'}"
+      style="width: 100%"
+      shadow="always"
+  >
+    <el-row>
+      <el-col :span="12">
+        <img class="image"
+             src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+      </el-col>
+      <el-col :span="12">
+        <el-row>
+          {{ this.customer }}
+        </el-row>
+        <el-row>
+          {{ this.customer }}
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: ['customer']
+}
+</script>
+
+<style scoped>
+
+</style>
